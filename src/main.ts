35010a820a8c5d63dff4d44a8749e89e030c8b50
Paulo Galarza <paulo-123@hotmail.es>
2026-06-01 08:21:56 -0500

    feat: OCP

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import './style.css'
+// import './solid/01-srp'
+import './solid/02-open-close-a'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
---
+++ src/solid/02-open-close-b.ts
@@ -0,0 +1,31 @@
+// Hay que agregar la dependencia de axios ```npm install axios```
+/* import axios from 'axios';*/
+import { HttpClient } from './02-open-close-c';
+
+export class TodoService { 
+    constructor(private httpClient: HttpClient) {}
+
+    async getTodoItems() {
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/todos/');
+        return data;
+    }
+}
+
+
+export class PostService {
+    constructor(private httpClient: HttpClient) {}
+    async getPosts() {
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/posts');
+        return data;
+    }
+}
+
+
+export class PhotosService {
+    constructor(private httpClient: HttpClient) {}
+    async getPhotos() {
+        const { data } = await this.httpClient.get('https://jsonplaceholder.typicode.com/photos');
+        return data;
+    }
+
+}
